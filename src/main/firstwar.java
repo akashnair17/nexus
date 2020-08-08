404af3bc31bfb46fb848e9db6f68d496e854ed08
kash <akashjaikumar17@gmail.com>
2020-08-07 18:12:59 -0700

    first

---
+++ src/main/firstwar.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      // Set response content type
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      out.println("Hello World");
+  }
+
